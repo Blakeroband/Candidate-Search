9cd72af732fad58ab482e86414ddf6c209e1a3c7
B A <blakeroband@gmail.com>
2025-02-10 17:30:04 -0600

    undid some changes, got API token

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,20 +1,7 @@
-import { Link } from "react-router-dom";
-
 const Nav = () => {
   // TODO: Add necessary code to display the navigation bar and link between the pages
   return (
-    <header>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="\Develop\src\pages\CandidateSearch.tsx">Potential Candidates</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <div>Nav</div>
   )
 };
 
--- src/interfaces/Candidate.interface.tsx
+++ src/interfaces/Candidate.interface.tsx
@@ -1,1 +1,8 @@
 // TODO: Create an interface for the Candidate objects returned by the API
+export default interface Candidate {
+  name: string;
+  location: string;
+  email: string;
+  company: string;
+  bio: string;
+}
